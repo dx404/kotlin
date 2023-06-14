bc2afeb1aa78bbc79b663cf77a043c6908fc972e
Sergej Jaskiewicz <sergej.jaskiewicz@jetbrains.com>
2023-06-14 13:04:50 +0200

    [IR] Reorder parameters in IrFactory#createTypeParameter
    This is to prepare for IrFactory auto-generation (KT-59308).

--- compiler/fir/fir2ir/src/org/jetbrains/kotlin/fir/backend/Fir2IrClassifierStorage.kt
+++ compiler/fir/fir2ir/src/org/jetbrains/kotlin/fir/backend/Fir2IrClassifierStorage.kt
@@ -436,10 +436,14 @@ class Fir2IrClassifierStorage(
                             symbolFactory = { IrTypeParameterPublicSymbolImpl(signature) }
                         ) { symbol ->
                             irFactory.createTypeParameter(
-                                startOffset, endOffset, origin, symbol,
-                                name, if (index < 0) 0 else index,
-                                isReified,
-                                variance
+                                startOffset = startOffset,
+                                endOffset = endOffset,
+                                origin = origin,
+                                name = name,
+                                symbol = symbol,
+                                variance = variance,
+                                index = if (index < 0) 0 else index,
+                                isReified = isReified,
                             )
                         }
                     } else {
@@ -448,19 +452,27 @@ class Fir2IrClassifierStorage(
                             symbolFactory = { IrTypeParameterPublicSymbolImpl(signature) }
                         ) { symbol ->
                             irFactory.createTypeParameter(
-                                startOffset, endOffset, origin, symbol,
-                                name, if (index < 0) 0 else index,
-                                isReified,
-                                variance
+                                startOffset = startOffset,
+                                endOffset = endOffset,
+                                origin = origin,
+                                name = name,
+                                symbol = symbol,
+                                variance = variance,
+                                index = if (index < 0) 0 else index,
+                                isReified = isReified,
                             )
                         }
 
                     }
                 } ?: irFactory.createTypeParameter(
-                    startOffset, endOffset, origin, IrTypeParameterSymbolImpl(),
-                    name, if (index < 0) 0 else index,
-                    isReified,
-                    variance
+                    startOffset = startOffset,
+                    endOffset = endOffset,
+                    origin = origin,
+                    name = name,
+                    symbol = IrTypeParameterSymbolImpl(),
+                    variance = variance,
+                    index = if (index < 0) 0 else index,
+                    isReified = isReified,
                 )
             }
         }
