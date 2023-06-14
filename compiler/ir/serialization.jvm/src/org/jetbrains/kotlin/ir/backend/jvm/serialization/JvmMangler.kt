1a8785cef42d88ce85fdbae15dec68816dd54336
Alexander Udalov <alexander.udalov@jetbrains.com>
2023-06-15 00:54:19 +0200

    JVM IR: fix fields incorrectly recognized as Java in IdSignature
    This was mentioned in KT-57754 and it's probably not a big problem, but
    it's easier to fix it than to think what it can break.

--- compiler/ir/serialization.jvm/src/org/jetbrains/kotlin/ir/backend/jvm/serialization/JvmMangler.kt
+++ compiler/ir/serialization.jvm/src/org/jetbrains/kotlin/ir/backend/jvm/serialization/JvmMangler.kt
@@ -15,17 +15,20 @@ import org.jetbrains.kotlin.backend.common.serialization.mangle.descriptor.Descr
 import org.jetbrains.kotlin.backend.common.serialization.mangle.ir.IrBasedKotlinManglerImpl
 import org.jetbrains.kotlin.backend.common.serialization.mangle.ir.IrExportCheckerVisitor
 import org.jetbrains.kotlin.backend.common.serialization.mangle.ir.IrMangleComputer
-import org.jetbrains.kotlin.descriptors.*
+import org.jetbrains.kotlin.descriptors.DeclarationDescriptor
+import org.jetbrains.kotlin.descriptors.FunctionDescriptor
+import org.jetbrains.kotlin.descriptors.ModuleDescriptor
+import org.jetbrains.kotlin.descriptors.PropertyDescriptor
 import org.jetbrains.kotlin.idea.MainFunctionDetector
 import org.jetbrains.kotlin.ir.declarations.IrDeclaration
 import org.jetbrains.kotlin.ir.declarations.IrFunction
+import org.jetbrains.kotlin.ir.descriptors.IrBasedFieldDescriptor
 import org.jetbrains.kotlin.ir.types.IrType
 import org.jetbrains.kotlin.ir.util.hasAnnotation
+import org.jetbrains.kotlin.ir.util.isFromJava
 import org.jetbrains.kotlin.load.java.JvmAnnotationNames
-import org.jetbrains.kotlin.load.java.lazy.descriptors.isJavaField
 import org.jetbrains.kotlin.load.java.typeEnhancement.hasEnhancedNullability
 import org.jetbrains.kotlin.types.KotlinType
-import org.jetbrains.kotlin.types.UnwrappedType
 import org.jetbrains.kotlin.types.checker.SimpleClassicTypeSystemContext
 
 object JvmIrMangler : IrBasedKotlinManglerImpl() {
@@ -78,6 +81,9 @@ class JvmDescriptorMangler(private val mainDetector: MainFunctionDetector?) : De
             // For more details see JvmPlatformOverloadsSpecificityComparator.kt
             return if (isJavaField) MangleConstant.JAVA_FIELD_SUFFIX else null
         }
+
+        private val PropertyDescriptor.isJavaField: Boolean
+            get() = getter == null && (this as? IrBasedFieldDescriptor)?.owner?.isFromJava() == true
 
         override fun visitModuleDeclaration(descriptor: ModuleDescriptor) {
             // In general, having module descriptor as `containingDeclaration` for regular declaration is considered an error (in JS/Native)
