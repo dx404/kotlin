41a9b9352ae10bc525ce2a15257d214a4f6d388e
Vladimir Sukharev <vladimir.sukharev@jetbrains.com>
2023-06-23 15:13:23 +0200

    [Test] Convert IGNORE: NATIVE directives in rest of tests
    ^KT-59057 Fixed

--- compiler/testData/codegen/box/enum/enumEntriesNameClashes.kt
+++ compiler/testData/codegen/box/enum/enumEntriesNameClashes.kt
@@ -1,4 +1,5 @@
 // !LANGUAGE: +EnumEntries
+// K2/NATIVE java.lang.IllegalArgumentException: IrErrorCallExpressionImpl(355, 362, "Unsupported callable reference: Q|box/enum/enumEntriesNameClashes/EnumWithClash|::R|box/enum/enumEntriesNameClashes/EnumWithClash.entries|") found but error code is not allowed
 // IGNORE_BACKEND_K2: JVM_IR, JS_IR, NATIVE
 // IGNORE_BACKEND: JS, JVM
 // WITH_STDLIB
